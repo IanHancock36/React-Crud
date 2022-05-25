cda01b100ba6ddb551358b48d3986fb3e4c27c5e
IanHancock36 <northroadian@gmail.com>
2022-05-25 06:15:34 -0500

    crud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  // used to add todos
+  const [todos, setTodos] = React.useState([])
+  // keeps track of current todo gives string
+  const [todo, setTodo] = React.useState("")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        {/* targeting the event in the input and the value is the todo "hook" */}
+        <input type='text'onChange={(e)=> setTodo(e.target.value)}/>
+        <button type='submit'>Add Journal Item</button>
+      </form>
+  
     </div>
   );
 }
